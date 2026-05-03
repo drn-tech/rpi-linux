dc7fc3074b31d8acd792f42088cd10c39281890a
Martin Petzold <32323179+martin-petzold@users.noreply.github.com>
2026-05-03 15:19:35 +0200

    Update rpi-simple-soundcard.c
    Added formats and sig_bits.

--- sound/soc/bcm/rpi-simple-soundcard.c
+++ sound/soc/bcm/rpi-simple-soundcard.c
@@ -223,8 +223,10 @@ static int rpi_i2s_8ch_input_init(struct snd_soc_pcm_runtime *rtd)
 	struct snd_soc_dai *codec_dai = snd_soc_rtd_to_codec(rtd, 0);
 
 	 codec_dai->driver->capture.channels_max = 8;
-	 codec_dai->driver->capture.rates = SNDRV_PCM_RATE_8000_96000;
-
+	 codec_dai->driver->capture.rates        = SNDRV_PCM_RATE_8000_96000;
+	 codec_dai->driver->capture.formats      = SNDRV_PCM_FMTBIT_S32_LE
+	 codec_dai->driver->capture.sig_bits     = 24,
+ 
 	 return 0;
 }
 
